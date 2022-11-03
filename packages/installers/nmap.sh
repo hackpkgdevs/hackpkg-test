bef248e14d8ab7e608f12a4c987634aea420d444
Jocelyn <107489279+CyberJosie@users.noreply.github.com>
2022-11-03 08:41:55 -0700

    Create nmap.sh

---
+++ packages/installers/nmap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export HACKPKG_INSTALL_SRC="apt";
+
+if [[ "$(uname -n)" == "debian" ]]
+then
+  apt-get install -y nmap
+  export HACKPKG_INSTALL_DST="$(which nmap)"
+else
+  export HACKPKG_ERROR="unsupported_distribution"
+fi
